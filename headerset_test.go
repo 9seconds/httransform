16a1b6ebb5ce15742b661a999ab2c67f433c4061
9seconds <nineseconds@yandex.ru>
2018-12-01 10:35:55 +0300

    Add test set for header parsing

--- headerset_test.go
+++ headerset_test.go
@@ -135,6 +135,67 @@ func (suite *HeaderSetTestSuite) TestDeleteWriteRestore() {
 	suite.Equal(items[4].Value, []byte("val5"))
 }
 
+type ParseHeadersTestSuite struct {
+	suite.Suite
+
+	set *HeaderSet
+}
+
+func (suite *ParseHeadersTestSuite) SetupTest() {
+	suite.set = getHeaderSet()
+}
+
+func (suite *ParseHeadersTestSuite) TearDownTest() {
+	releaseHeaderSet(suite.set)
+}
+
+func (suite *ParseHeadersTestSuite) TestEmpty() {
+	suite.NotNil(parseHeaders(suite.set, nil))
+}
+
+func (suite *ParseHeadersTestSuite) TestOnlyFirstLine() {
+	suite.NotNil(parseHeaders(suite.set, []byte("GET / HTTP/1.1\r\n")))
+}
+
+func (suite *ParseHeadersTestSuite) TestNoHeaders() {
+	suite.Nil(parseHeaders(suite.set, []byte("GET / HTTP/1.1\r\n\r\n")))
+	suite.Len(suite.set.Items(), 0)
+}
+
+func (suite *ParseHeadersTestSuite) TestMalformedHeader() {
+	text := []byte(`GET / HTTP/1.1
+Header: Value
+H
+
+`)
+	suite.NotNil(parseHeaders(suite.set, text))
+}
+
+func (suite *ParseHeadersTestSuite) TestHeaderOrder() {
+	text := []byte(`GET / HTTP/1.1
+STR1: Value1
+Str2: value2
+Val3: xxx
+	lalala
+val3: 1
+
+`)
+	suite.Nil(parseHeaders(suite.set, text))
+
+	items := suite.set.Items()
+	suite.Len(items, 3)
+	suite.Equal(items[0].Key, []byte("STR1"))
+	suite.Equal(items[0].Value, []byte("Value1"))
+	suite.Equal(items[1].Key, []byte("Str2"))
+	suite.Equal(items[1].Value, []byte("value2"))
+	suite.Equal(items[2].Key, []byte("Val3"))
+	suite.Equal(items[2].Value, []byte("1"))
+}
+
 func TestHeaderSet(t *testing.T) {
 	suite.Run(t, &HeaderSetTestSuite{})
+}
+
+func TestParseHeaders(t *testing.T) {
+	suite.Run(t, &ParseHeadersTestSuite{})
 }

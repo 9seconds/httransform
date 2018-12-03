97997acb0d613b78d0b08461aa4176e93f941661
9seconds <nineseconds@yandex.ru>
2018-12-03 13:10:58 +0300

    Add description to errors.go file

--- errors.go
+++ errors.go
@@ -3,5 +3,8 @@ package httransform
 import "errors"
 
 var (
+	// ErrProxyAuthorization is the error for ProxyAuthorizationBasicLayer
+	// instance. If OnRequest callback of this method returns such an error,
+	// then OnResponse callback generates correct 407 response.
 	ErrProxyAuthorization = errors.New("Cannot authenticate proxy user")
 )

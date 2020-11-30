1ab4f22fb77d0b6a13b3e7c8e68f2435e54d3f0d
9seconds <nineseconds@yandex.ru>
2020-11-30 17:05:33 +0300

    Add tests for unhijacking

---
+++ conns/unhijack_test.go
@@ -0,0 +1,96 @@
+package conns_test
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/9seconds/httransform/v2/conns"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type MockedConn struct {
+	mock.Mock
+}
+
+func (m *MockedConn) Read(b []byte) (int, error) {
+	args := m.Called(b)
+
+	return args.Int(0), args.Error(1)
+}
+
+func (m *MockedConn) Write(b []byte) (int, error) {
+	args := m.Called(b)
+
+	return args.Int(0), args.Error(1)
+}
+
+func (m *MockedConn) Close() error {
+	return m.Called().Error(0)
+}
+
+func (m *MockedConn) LocalAddr() net.Addr {
+	return m.Called().Get(0).(net.Addr)
+}
+
+func (m *MockedConn) RemoteAddr() net.Addr {
+	return m.Called().Get(0).(net.Addr)
+}
+
+func (m *MockedConn) SetDeadline(tm time.Time) error {
+	return m.Called(tm).Error(0)
+}
+
+func (m *MockedConn) SetReadDeadline(tm time.Time) error {
+	return m.Called(tm).Error(0)
+}
+
+func (m *MockedConn) SetWriteDeadline(tm time.Time) error {
+	return m.Called(tm).Error(0)
+}
+
+type UnsafeMockedConn struct {
+	MockedConn
+}
+
+func (u *UnsafeMockedConn) UnsafeConn() net.Conn {
+	return u.Called().Get(0).(net.Conn)
+}
+
+type UnhijackTestSuite struct {
+	suite.Suite
+
+	raw     *MockedConn
+	wrapped *UnsafeMockedConn
+}
+
+func (suite *UnhijackTestSuite) SetupTest() {
+	suite.raw = &MockedConn{}
+	suite.wrapped = &UnsafeMockedConn{}
+
+	suite.wrapped.On("UnsafeConn").Return(suite.raw)
+}
+
+func (suite *UnhijackTestSuite) TearDownTest() {
+	suite.raw.AssertExpectations(suite.T())
+	suite.wrapped.AssertExpectations(suite.T())
+}
+
+func (suite *UnhijackTestSuite) TestKeepOpened() {
+	conns.FixHijackHandler(func(net.Conn) bool {
+		return false
+	})(suite.wrapped)
+}
+
+func (suite *UnhijackTestSuite) TestClose() {
+    suite.raw.On("Close").Return(nil)
+
+	conns.FixHijackHandler(func(net.Conn) bool {
+		return true
+	})(suite.wrapped)
+}
+
+func TestUnhijack(t *testing.T) {
+	suite.Run(t, &UnhijackTestSuite{})
+}

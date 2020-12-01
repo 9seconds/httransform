db8bf9aa23ccef63706fb914ff47453584e3015b
9seconds <nineseconds@yandex.ru>
2020-12-01 15:14:55 +0300

    Add tests for utils

---
+++ headers/utils_test.go
@@ -0,0 +1,32 @@
+package headers_test
+
+import (
+	"testing"
+
+	"github.com/9seconds/httransform/v2/headers"
+	"github.com/stretchr/testify/suite"
+)
+
+type ValuesTestSuite struct {
+	suite.Suite
+}
+
+func (suite *ValuesTestSuite) TestNothing() {
+	suite.Empty(headers.Values(""))
+}
+
+func (suite *ValuesTestSuite) TestSingle() {
+	suite.Equal([]string{"value"}, headers.Values("value"))
+}
+
+func (suite *ValuesTestSuite) TestMany() {
+	suite.Equal([]string{"value", "hello"}, headers.Values("value, hello"))
+}
+
+func (suite *ValuesTestSuite) TestManySpace() {
+	suite.Equal([]string{"value   ", "hello"}, headers.Values("value  , hello"))
+}
+
+func TestValues(t *testing.T) {
+	suite.Run(t, &ValuesTestSuite{})
+}

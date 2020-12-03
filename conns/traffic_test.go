e41fe11499f971b25a64ec87aacf062fded32341
9seconds <nineseconds@yandex.ru>
2020-12-03 14:42:12 +0300

    Update tests for conns

--- conns/traffic_test.go
+++ conns/traffic_test.go
@@ -8,46 +8,69 @@ import (
 
 	"github.com/9seconds/httransform/v2/conns"
 	"github.com/9seconds/httransform/v2/events"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
+
+type EventChannelMock struct {
+	mock.Mock
+}
+
+func (e *EventChannelMock) Send(ctx context.Context, eventType events.EventType, value interface{}, shardKey string) {
+	e.Called(ctx, eventType, value, shardKey)
+}
 
 type TraffcConnTestSuite struct {
 	suite.Suite
 
-	cancel  context.CancelFunc
-	channel chan *events.Event
-	addr    *net.TCPAddr
-	raw     *MockedConn
-	conn    *conns.TrafficConn
+	cancel           context.CancelFunc
+	eventChannelMock *EventChannelMock
+	addr             *net.TCPAddr
+	raw              *MockedConn
+	conn             *conns.TrafficConn
 }
 
 func (suite *TraffcConnTestSuite) SetupTest() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	suite.cancel = cancel
-	suite.channel = make(chan *events.Event, 1)
+	suite.eventChannelMock = &EventChannelMock{}
 	suite.raw = &MockedConn{}
 	suite.addr = &net.TCPAddr{
 		IP:   net.ParseIP("127.0.0.1"),
 		Port: 1000,
 	}
 	suite.conn = &conns.TrafficConn{
-		Conn:    suite.raw,
-		Context: ctx,
-		ID:      "HELLO",
-		Events:  suite.channel,
+		Conn:        suite.raw,
+		Context:     ctx,
+		ID:          "HELLO",
+		EventStream: suite.eventChannelMock,
 	}
 }
 
 func (suite *TraffcConnTestSuite) TearDownTest() {
 	suite.cancel()
 	suite.raw.AssertExpectations(suite.T())
+	suite.eventChannelMock.AssertExpectations(suite.T())
 }
 
 func (suite *TraffcConnTestSuite) TestReadOk() {
 	suite.raw.On("Read", []byte(nil)).Once().Return(10, nil)
 	suite.raw.On("RemoteAddr").Return(suite.addr)
 	suite.raw.On("Close").Return(nil)
+	suite.eventChannelMock.On("Send",
+		mock.Anything,
+		events.EventTypeTraffic,
+		mock.AnythingOfType("*events.TrafficMeta"),
+		mock.Anything,
+	).Once().Run(func(args mock.Arguments) {
+		meta := args.Get(2).(*events.TrafficMeta)
+
+		suite.Equal(suite.conn.ID, meta.ID)
+		suite.Equal(suite.addr, meta.Addr)
+		suite.EqualValues(10, meta.ReadBytes)
+		suite.EqualValues(0, meta.WrittenBytes)
+	})
 
 	n, err := suite.conn.Read(nil)
 
@@ -55,26 +78,25 @@ func (suite *TraffcConnTestSuite) TestReadOk() {
 	suite.NoError(err)
 
 	suite.NoError(suite.conn.Close())
-
-	var meta *events.TrafficMeta
-
-	select {
-	case evt := <-suite.channel:
-		meta = evt.Value().(*events.TrafficMeta)
-	default:
-		suite.FailNow("Haven't found an event")
-	}
-
-	suite.Equal(suite.conn.ID, meta.ID)
-	suite.Equal(suite.addr, meta.Addr)
-	suite.EqualValues(10, meta.ReadBytes)
-	suite.EqualValues(0, meta.WrittenBytes)
 }
 
 func (suite *TraffcConnTestSuite) TestReadErr() {
 	suite.raw.On("Read", []byte(nil)).Once().Return(10, io.EOF)
 	suite.raw.On("RemoteAddr").Return(suite.addr)
 	suite.raw.On("Close").Return(nil)
+	suite.eventChannelMock.On("Send",
+		mock.Anything,
+		events.EventTypeTraffic,
+		mock.AnythingOfType("*events.TrafficMeta"),
+		mock.Anything,
+	).Once().Run(func(args mock.Arguments) {
+		meta := args.Get(2).(*events.TrafficMeta)
+
+		suite.Equal(suite.conn.ID, meta.ID)
+		suite.Equal(suite.addr, meta.Addr)
+		suite.EqualValues(10, meta.ReadBytes)
+		suite.EqualValues(0, meta.WrittenBytes)
+	})
 
 	n, err := suite.conn.Read(nil)
 
@@ -82,26 +104,25 @@ func (suite *TraffcConnTestSuite) TestReadErr() {
 	suite.Error(io.EOF, err)
 
 	suite.NoError(suite.conn.Close())
-
-	var meta *events.TrafficMeta
-
-	select {
-	case evt := <-suite.channel:
-		meta = evt.Value().(*events.TrafficMeta)
-	default:
-		suite.FailNow("Haven't found an event")
-	}
-
-	suite.Equal(suite.conn.ID, meta.ID)
-	suite.Equal(suite.addr, meta.Addr)
-	suite.EqualValues(10, meta.ReadBytes)
-	suite.EqualValues(0, meta.WrittenBytes)
 }
 
 func (suite *TraffcConnTestSuite) TestWriteOk() {
 	suite.raw.On("Write", []byte(nil)).Once().Return(10, nil)
 	suite.raw.On("RemoteAddr").Return(suite.addr)
 	suite.raw.On("Close").Return(nil)
+	suite.eventChannelMock.On("Send",
+		mock.Anything,
+		events.EventTypeTraffic,
+		mock.AnythingOfType("*events.TrafficMeta"),
+		mock.Anything,
+	).Once().Run(func(args mock.Arguments) {
+		meta := args.Get(2).(*events.TrafficMeta)
+
+		suite.Equal(suite.conn.ID, meta.ID)
+		suite.Equal(suite.addr, meta.Addr)
+		suite.EqualValues(0, meta.ReadBytes)
+		suite.EqualValues(10, meta.WrittenBytes)
+	})
 
 	n, err := suite.conn.Write(nil)
 
@@ -109,26 +130,25 @@ func (suite *TraffcConnTestSuite) TestWriteOk() {
 	suite.NoError(err)
 
 	suite.NoError(suite.conn.Close())
-
-	var meta *events.TrafficMeta
-
-	select {
-	case evt := <-suite.channel:
-		meta = evt.Value().(*events.TrafficMeta)
-	default:
-		suite.FailNow("Haven't found an event")
-	}
-
-	suite.Equal(suite.conn.ID, meta.ID)
-	suite.Equal(suite.addr, meta.Addr)
-	suite.EqualValues(0, meta.ReadBytes)
-	suite.EqualValues(10, meta.WrittenBytes)
 }
 
 func (suite *TraffcConnTestSuite) TestWriteErr() {
 	suite.raw.On("Write", []byte(nil)).Once().Return(10, io.EOF)
 	suite.raw.On("RemoteAddr").Return(suite.addr)
 	suite.raw.On("Close").Return(nil)
+	suite.eventChannelMock.On("Send",
+		mock.Anything,
+		events.EventTypeTraffic,
+		mock.AnythingOfType("*events.TrafficMeta"),
+		mock.Anything,
+	).Once().Run(func(args mock.Arguments) {
+		meta := args.Get(2).(*events.TrafficMeta)
+
+		suite.Equal(suite.conn.ID, meta.ID)
+		suite.Equal(suite.addr, meta.Addr)
+		suite.EqualValues(0, meta.ReadBytes)
+		suite.EqualValues(10, meta.WrittenBytes)
+	})
 
 	n, err := suite.conn.Write(nil)
 
@@ -136,20 +156,6 @@ func (suite *TraffcConnTestSuite) TestWriteErr() {
 	suite.Error(io.EOF, err)
 
 	suite.NoError(suite.conn.Close())
-
-	var meta *events.TrafficMeta
-
-	select {
-	case evt := <-suite.channel:
-		meta = evt.Value().(*events.TrafficMeta)
-	default:
-		suite.FailNow("Haven't found an event")
-	}
-
-	suite.Equal(suite.conn.ID, meta.ID)
-	suite.Equal(suite.addr, meta.Addr)
-	suite.EqualValues(0, meta.ReadBytes)
-	suite.EqualValues(10, meta.WrittenBytes)
 }
 
 func (suite *TraffcConnTestSuite) TestMixed() {
@@ -157,34 +163,31 @@ func (suite *TraffcConnTestSuite) TestMixed() {
 	suite.raw.On("Read", []byte(nil)).Once().Return(5, nil)
 	suite.raw.On("RemoteAddr").Return(suite.addr)
 	suite.raw.On("Close").Return(nil)
+	suite.eventChannelMock.On("Send",
+		mock.Anything,
+		events.EventTypeTraffic,
+		mock.AnythingOfType("*events.TrafficMeta"),
+		mock.Anything,
+	).Once().Run(func(args mock.Arguments) {
+		meta := args.Get(2).(*events.TrafficMeta)
+
+		suite.EqualValues(5, meta.ReadBytes)
+		suite.EqualValues(20, meta.WrittenBytes)
+	})
 
 	_, err := suite.conn.Write(nil)
 
-    suite.NoError(err)
+	suite.NoError(err)
 
 	_, err = suite.conn.Read(nil)
 
-    suite.NoError(err)
+	suite.NoError(err)
 
 	_, err = suite.conn.Write(nil)
 
-    suite.NoError(err)
-    suite.NoError(suite.conn.Close())
-    suite.NoError(suite.conn.Close())
-
-	var meta *events.TrafficMeta
-
-	select {
-	case evt := <-suite.channel:
-		meta = evt.Value().(*events.TrafficMeta)
-	default:
-		suite.FailNow("Haven't found an event")
-	}
-
-	suite.EqualValues(5, meta.ReadBytes)
-	suite.EqualValues(20, meta.WrittenBytes)
-
-    suite.Empty(suite.channel)
+	suite.NoError(err)
+	suite.NoError(suite.conn.Close())
+	suite.NoError(suite.conn.Close())
 }
 
 func TestTrafficConn(t *testing.T) {

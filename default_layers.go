f64f67fa55690062c5d51003d55987eba349c703
9seconds <nineseconds@yandex.ru>
2021-01-11 11:44:24 +0300

    Update default layers

--- default_layers.go
+++ default_layers.go
@@ -2,27 +2,17 @@ package httransform
 
 import (
 	"github.com/9seconds/httransform/v2/errors"
-	"github.com/9seconds/httransform/v2/headers"
 	"github.com/9seconds/httransform/v2/layers"
 )
 
 type layerStartHeaders struct{}
 
 func (l layerStartHeaders) OnRequest(ctx *layers.Context) error {
-	requestHeaders := headers.NewRequestHeaderWrapper(&ctx.Request().Header)
-
-	if err := ctx.RequestHeaders.Init(requestHeaders); err != nil {
-		return &errors.Error{
-			Message: "cannot read request headers",
-			Err:     err,
-		}
-	}
-
 	return nil
 }
 
 func (l layerStartHeaders) OnResponse(ctx *layers.Context, err error) error {
-	if err2 := ctx.ResponseHeaders.Sync(); err2 != nil {
+	if err2 := ctx.ResponseHeaders.Push(); err2 != nil {
 		return &errors.Error{
 			Message: "cannot sync response headers",
 			Err:     err,
@@ -35,7 +25,7 @@ func (l layerStartHeaders) OnResponse(ctx *layers.Context, err error) error {
 type layerFinishHeaders struct{}
 
 func (l layerFinishHeaders) OnRequest(ctx *layers.Context) error {
-	if err := ctx.RequestHeaders.Sync(); err != nil {
+	if err := ctx.RequestHeaders.Push(); err != nil {
 		return &errors.Error{
 			Message: "cannot sync request headers",
 			Err:     err,
@@ -46,9 +36,7 @@ func (l layerFinishHeaders) OnRequest(ctx *layers.Context) error {
 }
 
 func (l layerFinishHeaders) OnResponse(ctx *layers.Context, err error) error {
-	responseHeaders := headers.NewResponseHeaderWrapper(&ctx.Response().Header)
-
-	if err2 := ctx.ResponseHeaders.Init(responseHeaders); err2 != nil {
+	if err2 := ctx.ResponseHeaders.Pull(); err2 != nil {
 		return &errors.Error{
 			Message: "cannot read response headers",
 			Err:     err,
